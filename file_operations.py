ab143294c5bc38b39ff3fcdc5c43b5b80db25850
Aditya Dixit <qwertyuiop.dixit3@gmail.com>
2023-10-08 00:08:20 +0530

    added support for output file

--- file_operations.py
+++ file_operations.py
@@ -1,3 +1,14 @@
+import os
+import textwrap
+
 def read_request_from_file(file_path):
     with open(file_path, "r") as request_file:
         return request_file.read()
+
+def write_result_to_file(file_path, content):
+    cleaned_html_content = textwrap.dedent(content).strip()
+    if not os.path.exists("results"):
+        os.makedirs("results")
+    
+    with open(os.path.join("results", file_path), "w") as result_file:
+        result_file.write(cleaned_html_content)
--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-import os
-import textwrap
 import argparse
-from file_operations import read_request_from_file
+from file_operations import read_request_from_file, write_result_to_file
 from request_parser import parse_request
 from html_generator import generate_html_form
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate an HTML form from an HTTP request.")
+    parser = argparse.ArgumentParser(description="SeeAssArrAff PoC Creator")
     parser.add_argument("request_file", help="Path to the HTTP request file")
+    parser.add_argument("output_file", help="Path to the output CSRF PoC")
     args = parser.parse_args()
+
+    out_file = args.output_file
+    if "html" not in out_file:
+        out_file += ".html"
 
     request_string = read_request_from_file(args.request_file)
     method, full_url, headers, parsed_body = parse_request(request_string)
     html_content = generate_html_form(method, full_url, headers, parsed_body)
 
-    if not os.path.exists("results"):
-        os.makedirs("results")
+    write_result_to_file(out_file, html_content)
 
-    cleaned_html_content = textwrap.dedent(html_content).strip()
-    with open("results/output.html", "w") as html_file:
-        html_file.write(cleaned_html_content)
-
-    print(cleaned_html_content)
-    print("\nSeeAssArrAff PoC saved inside results/output.html")
+    print(html_content)
+    print(f"\nSeeAssArrAff PoC saved inside ./results/{out_file}")
 
 if __name__ == "__main__":
     main()
